629cc880bf6f895ad5ea45e67aa762c4961aec86
miguelrisquelme <miguelnascimento22@hotmail.com>
2020-01-24 19:53:10 -0300

    feito a doc do projeto | add novas funções

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # funcoes-matematicas
 É um pacote para JavaScript e Node.js com funções matemáticas prontas.
+
+## Documentação
+### Todas as funções estão em português (BR)
+
+1. Para calcular qualquer coisa basta após o ponto escrever calcular que aparecerá uma lista com todas as funções que retornam o cálculo
+2. Para verificar algo basta escrever verificar depois do ponto que você terá uma lista de todas as funções de verificação
--- index.js
+++ index.js
@@ -1,20 +1,60 @@
-exports.Soma = function(numero1, numero2){
+exports.calcularSoma = function(numero1, numero2){
     return numero1 + numero2;
 }
 
-exports.Subtracao = function(numero1, numero2) {
+exports.calcularSubtracao = function(numero1, numero2) {
     return numero1 - numero2;
 }
 
-exports.Multiplicacao = function(numero1, numero2) {
+exports.calcularMultiplicacao = function(numero1, numero2) {
     return numero1 * numero2;
 }
 
-exports.Divisao = function(numero1, numero2) {
+exports.calcularDivisao = function(numero1, numero2) {
     return numero1 * numero2;
 }
 
-exports.NumeroPar = function(numero) {
+exports.calcularAreaQuadrado = function(lado1) {
+    return lado1 * lado1;
+}
+
+exports.calcularAreaRetangulo = function(lado1, lado2) {
+    return lado1 * lado2;
+}
+
+exports.calcularAreaParalelogramo = function(base, altura){
+    return base * altura;
+}
+
+exports.calcularAreaTrianguloRetangulo = function(base, altura) {
+    return (base * altura) / 2;
+}
+
+exports.calcularAreaTrianguloEquilatero = function(lado){
+    return ((Math.sqrt(3) / 4) * (lado * lado));
+}
+
+exports.calcularAreaTrianguloIsosceles = function(base, altura){
+    return (base * altura) / 2;
+}
+
+exports.calcularAreaTrianguloEscaleno = function(lado1, lado2, lado3){
+    return (1/2 * lado2 * lado3 * Math.sin(lado1));
+}
+
+exports.calcularAreaLosango = function(diagonalMaior, diagonalMenor){
+    return (diagonalMaior * diagonalMenor) / 2;
+}
+
+exports.calcularAreaTrapezio = function(baseMaior, baseMenor, altura){
+    return ((baseMaior + baseMenor) * altura) / 2;
+}
+
+exports.calcularAreaCirculo = function(raio) {
+    return (3.14159 * (raio * raio));
+}
+
+exports.verificarNumeroPar = function(numero) {
     var resposta = false;
     if(numero % 2 == 0){
         resposta = true;
@@ -24,7 +64,7 @@ exports.NumeroPar = function(numero) {
     }
 }
 
-exports.NumeroImpar = function(numero) {
+exports.verificarNumeroImpar = function(numero) {
     var resposta = true;
     if(numero % 2 == 0){
         resposta = false;
