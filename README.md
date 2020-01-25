d3228ab8fcfa0a764523b08306441fe6749532f4
miguelrisquelme <miguelnascimento22@hotmail.com>
2020-01-24 23:55:34 -0300

    Novo tópico [funções de conversão] | atualizado README e outros

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-# funcoes-matematicas
-É um pacote para JavaScript e Node.js com funções matemáticas prontas.
+# Funções matemáticas
+Esse é um pacote para JavaScript e Node.js com funções matemáticas prontas, sejam elas de cálculo, conversão ou até mesmo verificação.
 
-## Documentação
-### Todas as funções estão em português (BR)
+## Documentação:
+### Todas as funções estão em português (BR) - O pacote estará recebendo atualizações sempre que possível.
+#### -> Veja as funções para saber seus parâmetros :) bom proveito.
+#### -> Para saber dos parâmetros das funções, em caso da IDE não der suporte, basta entrar na pasta do pacote e abrir o arquivo index.js, caso tenha dificuldade de achar a função desejada, basta apertar CTRL + F que dependendo da IDE abrirá um painel de busca, então busque pela palavra chave. O documento está inteiramente documentado, então será possível procurar pelos tópicos listados no mesmo.
 
-1. Para calcular qualquer coisa basta após o ponto escrever calcular que aparecerá uma lista com todas as funções que retornam o cálculo
-2. Para verificar algo basta escrever verificar depois do ponto que você terá uma lista de todas as funções de verificação
+1. Para calcular qualquer coisa basta após o ponto escrever <b>calcular</b> que aparecerá uma lista com todas as funções que fazem cálculos, o valor retornado é o resultado da conta;
+
+2. Para verificar algo basta escrever <b>verificar</b> depois do ponto que você terá uma lista de todas as funções de verificação e o que será retornado da função é um valor booleano.
+
+3. Para fazer a conversão de um número, basta após o ponto escrever <b>converter</b> que irá aparecer todas as funções de conversão disponiveis no momento, e o valor que retornar é o convertido com base no número que estava de parâmetro.
--- index.js
+++ index.js
@@ -1,59 +1,100 @@
+/**
+ * Desenvolvido e mantido por Miguel Riquelme
+ * 
+ * Linkedin: miguelrisquelme ou Miguel Riquelme
+ * Github: miguelrisquelme
+ * 
+ * Aperte CTRL + F para procurar pela palavra chave da função desejada
+ * 
+ * 
+ * Tópicos
+ * 1. Funções de cálculo
+ * 2. Funções de verificação
+ * 3. Funções de conversão
+ * 
+ * 
+ */
+
+
+
+
+// Funções de calculo
+
+// 1.
 exports.calcularSoma = function(numero1, numero2){
     return numero1 + numero2;
 }
 
+// 2.
 exports.calcularSubtracao = function(numero1, numero2) {
     return numero1 - numero2;
 }
 
+// 3.
 exports.calcularMultiplicacao = function(numero1, numero2) {
     return numero1 * numero2;
 }
 
+// 4.
 exports.calcularDivisao = function(numero1, numero2) {
     return numero1 * numero2;
 }
 
+// 5.
 exports.calcularAreaQuadrado = function(lado1) {
     return lado1 * lado1;
 }
 
+// 6.
 exports.calcularAreaRetangulo = function(lado1, lado2) {
     return lado1 * lado2;
 }
 
+// 7.
 exports.calcularAreaParalelogramo = function(base, altura){
     return base * altura;
 }
 
+// 8.
 exports.calcularAreaTrianguloRetangulo = function(base, altura) {
     return (base * altura) / 2;
 }
 
+// 9.
 exports.calcularAreaTrianguloEquilatero = function(lado){
     return ((Math.sqrt(3) / 4) * (lado * lado));
 }
 
+// 10.
 exports.calcularAreaTrianguloIsosceles = function(base, altura){
     return (base * altura) / 2;
 }
 
+// 11.
 exports.calcularAreaTrianguloEscaleno = function(lado1, lado2, lado3){
     return (1/2 * lado2 * lado3 * Math.sin(lado1));
 }
 
+// 12.
 exports.calcularAreaLosango = function(diagonalMaior, diagonalMenor){
     return (diagonalMaior * diagonalMenor) / 2;
 }
 
+// 13.
 exports.calcularAreaTrapezio = function(baseMaior, baseMenor, altura){
     return ((baseMaior + baseMenor) * altura) / 2;
 }
 
+// 14.
 exports.calcularAreaCirculo = function(raio) {
     return (3.14159 * (raio * raio));
 }
 
+
+
+// Funções de verificação
+
+// 1.
 exports.verificarNumeroPar = function(numero) {
     var resposta = false;
     if(numero % 2 == 0){
@@ -64,6 +105,7 @@ exports.verificarNumeroPar = function(numero) {
     }
 }
 
+// 2.
 exports.verificarNumeroImpar = function(numero) {
     var resposta = true;
     if(numero % 2 == 0){
@@ -72,4 +114,37 @@ exports.verificarNumeroImpar = function(numero) {
     } else {
         return resposta;
     }
+}
+
+
+// Funções de conversão
+
+// 1. 
+exports.converterQuilometroParaMetro = function(valorEmQuilometro){
+    return valorEmQuilometro * 1000;
+}
+
+// 2.
+exports.converterQuilometroParaCentimetro = function(valorEmQuilometro){
+    return valorEmQuilometro * 100000;
+}
+
+// 3.
+exports.converterMetroParaQuilometro = function(valorEmMetro){
+    return valorEmMetro / 1000;
+}
+
+// 4.
+exports.converterMetroParaCentimetro = function(valorEmMetro){
+    return valorEmMetro * 100;
+}
+
+// 5.
+exports.converterCentimetroParaQuilometro = function(valorEmCentimetro){
+    return valorEmCentimetro / 100000;
+}
+
+// 6.
+exports.converterCentimetroParaMetro = function(valorEmCentimetro){
+    return valorEmCentimetro / 100;
 }
